d60cb95ab6289e9ad677eed148c4289472c7314c
Brayan_Alvarez <10brayanalvarez@gmial.com>
2021-03-12 16:09:54 -0600

    ...

--- BD2_PARCIAL2_PROYECTO_ESCUELA/src/hn/uth/bd2/presentacion/frmPrincipal.java
+++ BD2_PARCIAL2_PROYECTO_ESCUELA/src/hn/uth/bd2/presentacion/frmPrincipal.java
@@ -172,7 +172,7 @@ public class frmPrincipal extends javax.swing.JFrame {
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         // TODO add your handling code here:
-        FrmPrimerGrado frm = new FrmPrimerGrado();
+        frmPrimerGrado frm = new frmPrimerGrado();
         escritorio.add(frm);
         frm.setVisible(true);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
